803bb5eb95b77d5127194185c1a7968098b914e0
IkerVera <106538772+IkerVera@users.noreply.github.com>
2022-05-30 07:09:31 -0700

    Add files via upload

---
+++ Python/1er Trimestre/1T,1T.py
@@ -0,0 +1,5 @@
+print('Hola Mundo')
+print('My name is Iker Santiago Vera Blanco')
+print('Im in 9C')
+print('I dont have favourite food but I like the Itialian Restaurants ')
+print('My favourite sport is the American footbal and I play it')
