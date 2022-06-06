7b02fa6ccc969338c01a959b352c03ca22e28f84
IkerVera <106538772+IkerVera@users.noreply.github.com>
2022-06-05 17:21:11 -0700

    Add files via upload

---
+++ Python/1er Trimestre/1T2T.py
@@ -0,0 +1,25 @@
+#hello there
+
+x=10
+jamon=10
+justo=2345
+excusa='puedo ir al baño'
+
+print(x)
+print(jamon)
+print(excusa)
+
+c0m03st4n=20
+print(c0m03st4n)
+print('el precio del jamon es :',jamon)
+
+#rehacer su presentacion ocupando variables
+
+nombre='My name is Iker Vera'
+print('me llamo', nombre)
+grade='9C'
+print('Im in', grade)
+favourite= 'Italian Food'
+print('My favourite food is',favourite)
+sport='My favourite sport is the American Football'
+print('Another thing of me is that', sport)
