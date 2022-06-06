064dbd35029daf8084db75ffab87ebe483339540
IkerVera <106538772+IkerVera@users.noreply.github.com>
2022-06-05 17:27:34 -0700

    Add files via upload

---
+++ Python/1er Trimestre/1T,3T.py
@@ -0,0 +1,59 @@
+trombones76='big parade'
+more=1000000
+classe='Advanced Theoretical Zyburgy'
+
+a=6
+b=2
+
+#asi se hace la suma +
+total=a+b
+print(total)
+
+#asi se hace la resta -
+total=a-b
+print(total)
+
+#asi se hace la multiplicacion *
+total=a*b
+print(total)
+
+#asi se hace la division /
+total=a/b
+print(total)
+
+total=a**b
+print(total)
+
+español1=9.3
+español2=8.9
+español3=9.2
+matematicas1=9
+matematicas2=8.7
+matematicas3=9.1
+historia1=8.5
+historia2=8.9
+histotia3=9.1
+FCyE1=10
+FCyE2=10
+FCyE3=10
+ingles1=9.1
+ingles2=9
+ingles3=8.9
+
+totalespañol=español1+español2+español3
+español=totalespañol/3
+
+totalmatematicas=matematicas1+matematicas2+matematicas3
+matematicas=totalmatematicas/3
+
+totalhistoria=historia1+historia2+histotia3
+historia=totalhistoria/3
+
+totalFCyE=FCyE1+FCyE2+FCyE3
+FCyE=totalFCyE/3
+
+totalingles=ingles1+ingles2+ingles3
+ingles=totalingles/3
+
+totalcalifications=español+matematicas+historia+FCyE+ingles
+finalgrade=total/5
