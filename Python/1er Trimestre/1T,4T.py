1f265aa12d67235559825be30317cb4456e61da5
IkerVera <106538772+IkerVera@users.noreply.github.com>
2022-06-05 17:30:18 -0700

    Add files via upload

---
+++ Python/1er Trimestre/1T,4T.py
@@ -0,0 +1,19 @@
+print("How we can act with empathy and positivity when we are online")
+print("trying to cheer up a person when you see him sad ")
+
+digital_dilema="a situation in digital life where its not always clear whats best to do"
+print(digital_dilema)
+
+opinion="I dont agree with the girl because she played with the feelings of the guy"
+print(opinion)
+
+identify="the girl and the boy, if the girl publish the letter or not"
+print(identify)
+
+feel="the boy:nervous, the girl: flattered, bucause the boy was sharing his feelings"
+print(feel)
+
+imagine="1rst option:the girl apologizing and trying to finish the problem, 2nd:the boy have never share the letter, 3rd: the girl never share it and doesnt say anything about the letter"
+print(imagine)
+
+say="3rd:maybe they could have been togheter or the girl say no but direct to him without saying anything"
