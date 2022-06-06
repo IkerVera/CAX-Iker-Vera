a3137095a0d1412f43107b710255aa933569d09b
IkerVera <106538772+IkerVera@users.noreply.github.com>
2022-06-05 17:32:52 -0700

    Add files via upload

---
+++ Python/1er Trimestre/1T;6T.py
@@ -0,0 +1,15 @@
+print('Whats your name:')
+name=input()
+print('hello',name)
+
+print('In wich grade are you:')
+grade=input()
+print('You are in',grade)
+
+print('What is your favourite food:')
+favouritefood=input()
+print('My favourite food its',favouritefood)
+
+print('Wich is your hobby:')
+hobby=input()
+print('My favourite hobby its,',hobby)
