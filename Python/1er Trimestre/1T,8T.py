4f0484b72e196be7d9a531255ed1161296f805c7
IkerVera <106538772+IkerVera@users.noreply.github.com>
2022-06-05 17:35:11 -0700

    Add files via upload

---
+++ Python/1er Trimestre/1T,8T.py
@@ -0,0 +1,8 @@
+numero=int(input('ingresa un numero:'))
+
+resultado=numero*20000
+print('el resultado es:', resultado)
+
+
+
+#classcode:preparen una calculadora de dos numeros, deberas sumar, restar, multiplicar y dividir
