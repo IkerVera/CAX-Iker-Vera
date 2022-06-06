047594d5a7b73969be3cbfaf041ed45dcbe17035
IkerVera <106538772+IkerVera@users.noreply.github.com>
2022-06-05 17:33:56 -0700

    Add files via upload

---
+++ Python/1er Trimestre/1T,7T.py
@@ -0,0 +1,42 @@
+print('Whats your name:')
+name=input()
+print('hello',name)
+
+print('In wich grade are you:')
+grade=input()
+print('You are in',grade)
+
+print('What is your favourite food:')
+favouritefood=input()
+print('My favourite food its',favouritefood)
+
+print('Wich is your hobby:')
+hobby=input()
+print('My favourite hobby its,',hobby)
+
+print('Wich sport you like:')
+sport=input()
+print('I like,',sport)
+
+print('Do you have brothers:')
+brothers=input()
+print('I have,',brothers)
+
+print('Do you have sisters:')
+sisters=input()
+print('I have,',sisters)  
+
+print('Who do you live with:')
+live=input()
+print('I live with,',live)
+
+print('How many years old are you:')
+years=input()
+print('I am,',years)
+
+print('Where do you born:')
+born=input()
+print('I born in,',born)
+
+
+ 
