fbab499ac2298c4f5b28ae867689c4c0f77959f0
IkerVera <106538772+IkerVera@users.noreply.github.com>
2022-06-27 09:37:19 -0700

    Add files via upload

---
+++ Python/2do Trimestre/2T,5T.py
@@ -0,0 +1,3 @@
+print("Este año voy a cumplir lo que me propomga")
+print("Entregar todo y ayudar a mis compañeros")
+print("poner mas atencion, emepezar a agarrar más comfinaza con la plataforma, mejorar la forma en que lo organizo")
