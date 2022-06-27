9cb8d2390b5c98aa5dd771d16085270e7da1c4bb
IkerVera <106538772+IkerVera@users.noreply.github.com>
2022-06-27 09:51:08 -0700

    Add files via upload

---
+++ Python/3er Trimestre/3T,3T.py
@@ -0,0 +1,21 @@
+while True:
+  x=input("ingresa la palabra:")
+  print(x)
+
+  if(x=="dale"):
+    break
+print("fin del juego")
+input()
+
+suma=9
+for x in range (100):
+  suma=suma+2
+  input()
+
+  if(suma==23):
+    break
+    #continue
+
+    print(suma)
+
+    #class review:
