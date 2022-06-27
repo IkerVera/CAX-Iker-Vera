dcd734641a005ddb66c2c91aa71966b33434e2f7
IkerVera <106538772+IkerVera@users.noreply.github.com>
2022-06-27 09:50:27 -0700

    Add files via upload

---
+++ Python/3er Trimestre/3T,1T.py
@@ -0,0 +1,8 @@
+x=int(input("ingrese x"))
+y=int(input("ingrese y"))
+
+if (x>9 and y==7):
+    print("ambas condiciones se cumplen")
+
+if (x==10 or y>1):
+    print("al menos una condicion se cumple")
