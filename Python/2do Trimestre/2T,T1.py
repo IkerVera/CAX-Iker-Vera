52137362119c0758f8d5479ad8107f465f700528
IkerVera <106538772+IkerVera@users.noreply.github.com>
2022-06-27 09:25:06 -0700

    Add files via upload

---
+++ Python/2do Trimestre/2T,T1.py
@@ -0,0 +1,17 @@
+numero=int(input('ingresa un numero:'))
+
+resultado=numero*2*3.14
+print('el area del circulo es :', resultado)
+
+numero=int(input('ingresa numero:'))
+numero2=int(input('ingresa un numero2:'))
+altura=int(input('ingresa la altura:'))
+resultado=numero+numero2*altura/2
+print('el area del trapezoide es :', resultado)
+
+numero=int(input('ingresa un numero:'))
+numero2=int(input('ingresa un segundo numero:'))
+
+resultado=numero+numero2/2
+print('el area del rombo es:', resultado)
+
