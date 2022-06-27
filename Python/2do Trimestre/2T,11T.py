b05ae757f5e1b959635f5830d114ee505cb5491c
IkerVera <106538772+IkerVera@users.noreply.github.com>
2022-06-27 09:39:09 -0700

    Add files via upload

---
+++ Python/2do Trimestre/2T,11T.py
@@ -0,0 +1,10 @@
+numero=int(input("ingresa un numero"))
+
+if (numero<8):
+    print("te falta chavo")
+
+#todo lo demas
+#para que haya un else necesitamos un if 
+else:
+    print("te sobra chavo")
+  
