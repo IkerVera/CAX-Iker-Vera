69fdacfd32ed9671338608826f7e00ec370ec3d1
IkerVera <106538772+IkerVera@users.noreply.github.com>
2022-06-27 09:51:30 -0700

    Add files via upload

---
+++ Python/3er Trimestre/3T,4T challenge.py
@@ -0,0 +1,17 @@
+while True:
+  print("Bienvenidos a Megaplay consultas")
+  input()
+  Consulta = (input('Cual es el motivo de su mensaje:a)Falla b)Pago c)Promo'))
+  if Consulta == "Falla":
+   print("Espere un momento")
+Falla = (input('Cual es su falla a)Television o b)Red'))
+if Falla == "Television":
+      print('Este es su numero de reporte:1234')
+      if Falla == "Red":
+        print('Este es su numero de reporte:4321')
+if Consulta == "Pago":
+ Tarjeta = int(input('Ingrese la su tarjeta de credito :'))
+Pago = int(input('Ingrese la contraseña su tarjeta de credito :'))
+print("Su pago a sido resivido")
+if Consulta == "Promo":
+  print( '1 mes gratis de datos ilimitados a solo $99.99, 12 meses sin intereses en la comrpra de un celular, 24 meses de seguro en un celular a 1000 al mes, a la compra de un celular iphone, te regalamos un chicle' )
